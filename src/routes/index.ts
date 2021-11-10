3658b35b46c313a83080db6e23bd7d00203ef9a9
苏小秋 <choxsu@163.com>
2021-11-10 17:30:02 +0800

    update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,44 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
-const routes: RouteRecordRaw[] = [
-    {
-        path: '/login',
-        component: () => import("@/views/login/index.vue")
-    },
-    {
-        path: '/',
-        component: () => import("@/views/home/index.vue")
-    },
-]
+const routes: RouteRecordRaw[] = []
+
+const modules = import.meta.glob("./modules/*.ts")
+// 遍历 modules 对象的 key 值来访问相应的模块
+for (const path in modules) {
+    const mod = await modules[path]()
+    // 数据添加到routes中
+    routes.push(...mod.default)
+}
+
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+
+
+router.beforeEach((to, format, next) => {
+    // 判断路由是否要去登录页面
+    // 是的话 清除sessionStorage存储 放行
+    // 否则进行验证，用户是否已经登录
+    // 有登陆记录的话放行
+    // 否则清除sessionStorage存储，并重定向到login
+    if (to.path === '/login') {
+        //清除sessionStorage存储
+        sessionStorage.clear()
+        next()
+    } else {
+        let user = sessionStorage.getItem('user')
+        if (user) {
+            next()
+        } else {
+            //清除sessionStorage存储
+            sessionStorage.clear()
+            // 重定向到login
+            next("/login?url=" + to.path)
+        }
+    }
 })
 
 
---
+++ src/routes/modules/home.ts
@@ -0,0 +1,9 @@
+import { RouteRecordRaw } from 'vue-router'
+const routes: RouteRecordRaw[] = [
+    {
+        path: '/',
+        component: () => import("@/views/home/index.vue")
+    },
+]
+
+export default routes
